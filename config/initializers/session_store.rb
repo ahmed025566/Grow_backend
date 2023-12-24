9288ecf84d734acf298091ff8254b3f73ff5374c
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-24 14:07:32 +0200

    Add api domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  # Rails.application.config.session_store :cookie_store, key: '_grow', expire_after: 1.hour, domain: 'our rails api production domain'
+  Rails.application.config.session_store :cookie_store, key: '_grow', expire_after: 1.hour, domain: 'grow-api-67iq.onrender.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_grow', expire_after: 1.hour
 end
