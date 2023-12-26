7b57c0d00b8e3ced026ed62fce6f12846b4e4917
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-26 15:39:17 +0200

    Fix session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_grow', expire_after: 1.hour, domain: 'https://grow-api-67iq.onrender.com'
+  Rails.application.config.session_store :cookie_store, key: '_grow', domain: 'https://grow-api-67iq.onrender.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_grow', expire_after: 1.hour
+  Rails.application.config.session_store :cookie_store, key: '_grow'
 end
 
