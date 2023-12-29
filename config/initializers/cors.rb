10f38a367eed154cb7728e45020790abc782e367
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-29 23:23:53 +0200

    Fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
    origins "https://grow-lqwz.onrender.com",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "http://localhost:3000"
             
     resource "*",
     headers: :any,
